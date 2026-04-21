965031f4d129c269589cbe366d49eaf8d17df8ee
atulsinghhhh <atulsinghhh25@gmail.com>
2026-04-21 16:07:28 +0530

    add redisinsight image in docker compose file

--- backend/src/utilis/redis.ts
+++ backend/src/utilis/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis"
 
 export const redis = createClient({
-    url: process.env.REDIS_URL || "redis://localhost:6379",
+    url: process.env.REDIS_URL || "redis://redis:6379",
 });
 
 redis.on("error",(error)=>{
@@ -10,5 +10,10 @@ redis.on("error",(error)=>{
 
 export async function connectRedis(){
     await redis.connect();
-    console.log("Redis Connected to:", process.env.REDIS_URL || "redis://localhost:6379");
+    console.log("Redis Connected to:", process.env.REDIS_URL || "redis://redis:6379");
 }
+
+/*
+    time="2026-04-21T13:53:13+05:30" level=warning msg="C:\\Users\\Atul_Rathore\\chaiorlearn\\urlshortener\\docker-compose.yaml: 
+    the attribute `version` is obsolete, it will be ignored, please remove it to avoid potential confusion"
+*/
